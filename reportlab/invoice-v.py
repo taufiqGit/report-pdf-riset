2cdc7977b0da05c52b782a33a3635e2c77ccf19d
taufiqGit <wecannn29@gmail.com>
2024-03-21 12:16:40 +0700

    -

--- reportlab/invoice-v.py
+++ reportlab/invoice-v.py
@@ -2,9 +2,24 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch 
 from reportlab.lib.pagesizes import letter, A4
 from temp_invoice import my_temp
+from invoice_data import my_sale, my_prod
 
 c = canvas.Canvas("./invoice-01.pdf", pagesize=letter)
 c = my_temp(c)
 
+c.setFillColorRGB(0,0,1) # font colour
+c.setFont("Helvetica", 20)
+row_gap=0.5 # gap between each row
+line_y=6.7 # location of fist Y position 
+total=0
+
+for items in my_sale:
+    c.drawString(0*inch, line_y*inch, str(my_prod[items][0]))
+    c.drawRightString(3.5*inch, line_y*inch, "$ " + str(my_prod[items][1]))
+    c.drawRightString(5*inch, line_y*inch, str(my_sale[items]))
+    sub_total = my_prod[items][1]*my_sale[items]
+    c.drawRightString(7*inch, line_y*inch, "$ " + str(sub_total))
+    total = round(total + sub_total, 1)
+    line_y = line_y - row_gap
 c.showPage()
 c.save()
---
+++ reportlab/invoice_data.py
@@ -0,0 +1,9 @@
+my_prod={1:['Hard Disk',80],2:['RAM',90],3:['Monitor',75],
+         4:['CPU',55],5:['Keyboard',20],6:['Mouse',10],
+         7:['Mother board',50],8:['Power Sypply',20],
+         9:['Speaker',50],10:['Microphone',45]}
+
+my_sale={1:2,3:2,7:1,4:3,6:5,5:3,2:1,9:1,10:3}
+
+discount_rate=10 # 10% discount 
+tax_rate=12 # tax rate  in percentage 
